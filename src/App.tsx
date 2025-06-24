d5d713ad78355a6c1db8556a45499635dae0bcf0
RoxanaCamara <roxana.camara07@gmail.com>
2025-06-23 23:28:33 -0300

    feat: blog card

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import viteLogo from "./assets/illustration-article.svg";
+import user from "./assets/user.avif";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="container__card">
+        <img src={viteLogo} className="card__image" alt="" />
+        <div className="card__section">
+          <button className="section__button">Learning</button>
+          <p className="section__fecha ">Published 21 Dec 2023</p>
+          <h2 className="section__title">HTML & CSS foundations</h2>
+          <p className="section__text">
+            These languages are the backbone of every website, defining
+            structure, content, and presentation.
+          </p>
+        </div>
+        <div className="card__footer">
+          <img src={user} className="footer__avatar" alt="" />
+          <p className="footer__name">Greg Hooper</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
